dfcea8d36f42bf88a5c7ae241fc30eb7edaca73c
huonglt <lthuongvn@yahoo.com>
2017-05-20 11:42:31 +1200

    config the app to use postcss

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from '../css/App.css';
 
 export default class App extends React.Component {
   render() {
+    let { container } = styles;
     return (
-      <div>
+      <div className={container}>
         <h1>A blank React + Webpack 2 + Yarn app</h1>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,19 @@ module.exports = {
 
 	module: {
 		loaders: [
-			{ test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ }
+			{ test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+			{ test: /\.css$/, use: ['style-loader',  {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              sourceMap: true,
+              importLoaders: 1,
+              localIdentName: '[name]--[local]--[hash:base64:8]'
+            }
+          },
+          'postcss-loader'
+        ]
+      }
 		]
 	},
 	plugins: [HtmlWebpackPluginConfig]
