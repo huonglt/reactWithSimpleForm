cf75399f810a4d2fb01a17b622cd73249d302c7f
huonglt <lthuongvn@yahoo.com>
2017-05-27 13:27:37 +1200

    use classnames for handling class names

--- client/components/TestRadio.js
+++ client/components/TestRadio.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+
 import styles from '../css/TestRadio.css';
 
 export default class TestRadio extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.getRadioButtonStyle = this.getRadioButtonStyle.bind(this);
     this.state = { checked: this.props.checked };
-
+    this.cx = classNames.bind(styles);
   }
   handleClick() {
     if(this.props.disabled) {
@@ -18,18 +21,24 @@ export default class TestRadio extends React.Component {
       this.setState({ checked: !this.state.checked});
     }
   }
-
-  render() {
+  getRadioButtonStyle() {
     /*
      * if the radio button is managed through the RadioGroup control, its checked property is determined by its props
      * If the radio button is a stand-alone one, its checked property is determined by its own state
     */
-    let propertyController = (this.props.handleClick) ? this.props : this.state;
-    let disabled = this.props.disabled;
-    let radio = styles.oval +  (propertyController.checked ? ' ' + styles.checked : '') + (disabled ? ' ' + styles.disabled : '');
+    const propertyController = (this.props.handleClick) ? this.props : this.state;
+
+    return this.cx({
+      oval: true,
+      checked: propertyController.checked,
+      disabled: this.props.disabled,
+    });
+  }
+  render() {
+    const radioButton = this.getRadioButtonStyle();
     return (
       <div onClick={this.handleClick}>
-        <div className={radio} ></div>
+        <div className={radioButton} ></div>
         <div className={styles.label}>{this.props.label}</div>
       </div>
     );
